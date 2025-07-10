6b94c22dedaeda20e329e780902662e3d3fa4962
KANTAPENG <kanta112233jw@gmail.com>
2025-07-10 11:50:26 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   validates :nickname,           presence: true
   validates :email,              presence: true
   validates :encrypted_password, presence: true
-  validates :first_name,         presence: true
-  validates :last_name,          presence: true
-  validates :first_name_kana,    presence: true
-  validates :last_name_kana,     presence: true
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角文字を使用してください" }
+  validates :last_name, presence: true,
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角文字を使用してください" }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー]+\z/, message: "は全角カタカナを使用してください" }
+  validates :last_name_kana, presence: true,
+                             format: { with: /\A[ァ-ヶー]+\z/, message: "は全角カタカナを使用してください" }
   validates :birthday,           presence: true
 end
