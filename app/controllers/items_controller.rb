d81edc20a7518468357cc7803b5249d72a62d57c
KANTAPENG <kanta112233jw@gmail.com>
2025-10-07 14:37:39 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,15 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create] 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
-
-   def index
+  def index
     @items = Item.all.order(created_at: :desc)
-   end
+  end
 
   def new
     @item = Item.new
   end
-
-   def show
-    @item = Item.find(params[:id])
-   end
 
   def create
     @item = Item.new(item_params)
@@ -23,7 +20,31 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: "商品情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @item.user_id || @item.shopping_record.present?
+      redirect_to root_path
+    end
+  end
 
   def item_params
     params.require(:item).permit(
@@ -35,7 +56,7 @@ class ItemsController < ApplicationController
       :prefecture_id, 
       :days_to_ship_id, 
       :price, 
-      :image 
+      :image
     ).merge(user_id: current_user.id)
   end
 end
