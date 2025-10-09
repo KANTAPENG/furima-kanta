cabd885d9deb81d6909138a80d47a94881e85d27
KANTAPENG <kanta112233jw@gmail.com>
2025-10-09 10:57:41 +0900

    commit

--- app/models/shopping_record.rb
+++ app/models/shopping_record.rb
@@ -1,6 +1,6 @@
-class ShoppingRecord < ApplicationRecord
+#class ShoppingRecord < ApplicationRecord
 
-  belongs_to :user
-  has_one :shopping_information
-  belongs_to :item
-end
+  #belongs_to :user
+  #has_one :shopping_information
+  #belongs_to :item
+#end
